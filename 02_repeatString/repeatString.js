ab4aba8431d9b88ec8e68568e66acc6c02c32d8c
alexsemendinger <66119933+alexsemendinger@users.noreply.github.com>
2024-11-11 12:15:42 -0500

    exercise 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, numRepeats) {
+    if (numRepeats < 0) return "ERROR";
+    let repeatedText = "";
+    for (let i = 0; i < numRepeats; i++) {
+        repeatedText += text;
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
