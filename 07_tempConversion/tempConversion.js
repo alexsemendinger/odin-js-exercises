78c42706f75764a81cc0baa4a250b778d2add834
alexsemendinger <66119933+alexsemendinger@users.noreply.github.com>
2024-11-11 14:39:53 -0500

    exercises 6 and 7 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrTemp) {
+  const celsiusTemp = (fahrTemp - 32) * 5 / 9;
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const fahrTemp = celsiusTemp * 9 / 5 + 32;
+  return Math.round(fahrTemp * 10) / 10;
 };
 
 // Do not edit below this line
