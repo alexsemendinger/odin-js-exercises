6c5044cbf3fa1b462d8e69cfe20bcad95a264618
alexsemendinger <66119933+alexsemendinger@users.noreply.github.com>
2024-11-11 14:04:06 -0500

    third exercise complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (const itemToRemove of args) {
+        array = array.filter(x => x !== itemToRemove);
+    }
+    return array
 };
+
+removeFromArray([1,2,3,4], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
