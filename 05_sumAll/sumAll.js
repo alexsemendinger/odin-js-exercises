d8675aa0238d741d592d0482754df4e898a2fe26
alexsemendinger <66119933+alexsemendinger@users.noreply.github.com>
2024-11-11 14:18:47 -0500

    initial passing solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ( typeof num1 != 'number' || typeof num2 != 'number') {
+        return "ERROR"
+    }
+    if ( num1 < 1 || num2 < 1 ) {
+        return "ERROR"
+    }
+    if ( num1 % 1 != 0 || num2 % 1 != 0 ) {
+        return "ERROR"
+    }
 
+    let low = (num1 < num2) ? num1 : num2;
+    let high = (num1 >= num2) ? num1 : num2;
+
+    let sum = 0;
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
